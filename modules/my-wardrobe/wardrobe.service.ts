cd3c01cb7b2e45c30155dcdbafad83bc42f4d687
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-03 21:27:08 +0200

    feat(wardrobe): add category validation in createWardrobeItemService

--- modules/my-wardrobe/wardrobe.service.ts
+++ modules/my-wardrobe/wardrobe.service.ts
@@ -6,12 +6,14 @@ import { ApiResponseSuccess } from "@/types/response.type";
 import { CreateWardrobeItemResponse } from "./types/dto.types";
 import { CreateWardrobeItemDTO } from "./types/dto.types";
 import userRepo from "../user/user.repo";
+import { findCategoryById } from "../category/category.repo";
 
 export const createWardrobeItemService = async (CreateWardrobeItemDTO: CreateWardrobeItemDTO): Promise<ApiResponseSuccess<CreateWardrobeItemResponse>> => {
   const data = zodValidation(CreateWardrobeItemDTOSchema, CreateWardrobeItemDTO);
   const { imageUrls, ...rest } = data;
 
   await userRepo.findById(rest.userId);
+  await findCategoryById(rest.categoryId);
 
   const wardrobeItem = await createWardrobeItemRepo(rest, imageUrls);
 
