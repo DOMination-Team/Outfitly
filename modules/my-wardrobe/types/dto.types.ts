909b1626a630d73bafd60db13e2c428c9b8dd05c
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-06 15:44:29 +0200

    feat: make update wardrobe item schema

--- modules/my-wardrobe/types/dto.types.ts
+++ modules/my-wardrobe/types/dto.types.ts
@@ -1,5 +1,7 @@
-import { WardrobeItem, WardrobeItemImage } from "@/app/generated/prisma/browser";
-import { WardrobeItemWithoutAddedAtAndId } from ".";
+import { WardrobeItemWithImages, WardrobeItemWithoutAddedAtAndId } from ".";
 
 export type CreateWardrobeItemDTO = WardrobeItemWithoutAddedAtAndId & { imageUrls: string[] };
-export type CreateWardrobeItemResponse = WardrobeItem & { images: WardrobeItemImage[] };
+export type CreateWardrobeItemResponse = WardrobeItemWithImages;
+
+export type UpdateWardrobeItemDTO = Partial<WardrobeItemWithoutAddedAtAndId & { imageUrls: string[] }> & { id: string, userId: string };
+export type UpdateWardrobeItemResponse = WardrobeItemWithImages;
