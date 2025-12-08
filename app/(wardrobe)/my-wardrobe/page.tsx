26741c84d0d83369056778f5e38046a60135bab2
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-08 09:42:48 +0200

    feat: add MyWardrobe component and integrate into MyWardrobePage

---
+++ app/(wardrobe)/my-wardrobe/page.tsx
@@ -0,0 +1,7 @@
+import MyWardrobe from "@/modules/wardrobe/my-wardrobe/components/myWardrobePage";
+
+const MyWardrobePage = () => {
+  return <MyWardrobe />;
+};
+
+export default MyWardrobePage;
---
+++ modules/wardrobe/my-wardrobe/components/myWardrobePage.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "@/components/page-header";
+
+const Home = () => {
+  return (
+    <main className="pt-20 pb-16">
+      <PageHeader title="My Wardrobe" subtitle="Manage and organize your fashion collection" />
+    </main>
+  );
+};
+
+export default Home;
