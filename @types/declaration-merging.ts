d806e3502701e84643a9bd7fe789b8b89d4a7c16
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-15 21:15:22 +0200

    feat: add S3_BUCKET_NAME env variable config with type safety

--- @types/declaration-merging.ts
+++ @types/declaration-merging.ts
@@ -1,10 +1,11 @@
 export interface MyEnvs {
   DATABASE_URL: string;
+  S3_BUCKET_NAME: string;
 }
 
 /* eslint-disable @typescript-eslint/no-namespace */
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends MyEnvs {}
+    interface ProcessEnv extends MyEnvs { }
   }
 }
