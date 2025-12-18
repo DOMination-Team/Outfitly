efd3f2e21b746277c3bb132a04aceabc00ad80a8
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-12-18 17:31:38 +0200

    feat: remove notification icon in navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -52,7 +52,7 @@ export function Navbar() {
 
             {/* Desktop CTA */}
             <div className="hidden lg:flex items-center gap-4">
-              <Link href="/notifications">
+              {/* <Link href="/notifications">
                 <Button
                   variant="ghost"
                   className="hover:bg-transparent p-2 relative"
@@ -71,7 +71,7 @@ export function Navbar() {
                     3
                   </span>
                 </Button>
-              </Link>
+              </Link> */}
 
               <CustomButton
                 variant="ghost"
@@ -107,7 +107,7 @@ export function Navbar() {
 
             {/* Mobile Icons */}
             <div className="lg:hidden flex items-center gap-2">
-              <Link href="/notifications" className="relative p-2">
+              {/* <Link href="/notifications" className="relative p-2">
                 <Bell className="w-5 h-5" style={{ color: NAVBAR_COLORS.link }} />
                 <span
                   className="absolute -top-1 -right-1 w-4 h-4 rounded-full text-xs flex items-center justify-center"
@@ -120,7 +120,7 @@ export function Navbar() {
                 >
                   3
                 </span>
-              </Link>
+              </Link> */}
 
               <CustomButton
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
