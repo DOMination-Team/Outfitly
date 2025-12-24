ffabf67e3a121c37393db715ae907f5c31b7d4d5
monazak <monazakout67@gmail.com>
2025-12-24 17:19:16 +0200

    fix:add avatar src attribute

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -14,16 +14,29 @@ const Avatar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElem
 );
 Avatar.displayName = "Avatar";
 
-const AvatarImage = React.forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(
-  ({ className, ...props }, ref) => (
+const AvatarImage = React.forwardRef<
+  HTMLImageElement,
+  React.ImgHTMLAttributes<HTMLImageElement>
+>(({ className, src, ...props }, ref) => {
+  if (!src) return null
+
+  return (
     // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-    <img ref={ref} className={cn("aspect-square h-full w-full", className)} {...props} />
-  ),
-);
-AvatarImage.displayName = "AvatarImage";
+    <img
+      ref={ref}
+      src={src}
+      className={cn("aspect-square h-full w-full", className)}
+      {...props}
+    />
+  )
+})
+
+AvatarImage.displayName = "AvatarImage"
+
 
 const AvatarFallback = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
+
     <div
       ref={ref}
       className={cn(
@@ -32,6 +45,8 @@ const AvatarFallback = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTM
       )}
       {...props}
     />
+
+
   ),
 );
 AvatarFallback.displayName = "AvatarFallback";
