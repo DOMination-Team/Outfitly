ad937630bb68e70a7b2ed9c5884cd93083506b64
monazak <monazakout67@gmail.com>
2025-12-26 16:45:10 +0200

    refactor(weatherIcon):move types into types file

--- modules/weather/components/weather-icon/weatherIcon.tsx
+++ modules/weather/components/weather-icon/weatherIcon.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Droplets, Wind, Eye } from "lucide-react";
-
-interface WeatherIconProps {
-  condition?: string;
-}
+import {WeatherIconProps} from './weatherIcon.types'
 
 export const WeatherIcon: React.FC<WeatherIconProps> = ({ condition }) => {
+ 
   const c = (condition || "").toLowerCase();
-
+ 
   if (c.includes("thunder")) return <Wind className="w-32 h-32" />;
   if (c.includes("rain") || c.includes("drizzle")) return <Droplets className="w-32 h-32" />;
   if (c.includes("snow")) return <Eye className="w-32 h-32" />;
